6d44749569be3c14781e693cfaa20af7dc1282e3
HealthAdvisor Developer <healthadvisor@example.com>
2025-11-02 15:13:20 +0700

    🔧 Fix: Thêm __init__.py cho diseases/respiratory và diseases/renal

---
+++ diseases/renal/__init__.py
@@ -0,0 +1,18 @@
+"""
+Renal disease modules
+"""
+
+from . import chronic_kidney_disease
+from . import kidney_stones
+from . import uti
+from . import nocturia
+from . import bph
+
+__all__ = [
+    'chronic_kidney_disease',
+    'kidney_stones',
+    'uti',
+    'nocturia',
+    'bph',
+]
+
---
+++ diseases/respiratory/__init__.py
@@ -0,0 +1,18 @@
+"""
+Respiratory disease modules
+"""
+
+from . import copd
+from . import asthma
+from . import pneumonia
+from . import chronic_cough
+from . import sleep_apnea
+
+__all__ = [
+    'copd',
+    'asthma',
+    'pneumonia',
+    'chronic_cough',
+    'sleep_apnea',
+]
+
