28325daa6b1b2db489976c54472fbb0cfe4731d9
HealthAdvisor Developer <healthadvisor@example.com>
2025-11-02 07:40:47 +0700

    feat: Thêm tab Ho Mãn Tính vào trang Hô Hấp

--- pages/2_🫁_Hô_Hấp.py
+++ pages/2_🫁_Hô_Hấp.py
@@ -9,7 +9,7 @@ sys.path.append('..')
 
 from core.ui_config import get_custom_css
 from core.sidebar_menu import render_sidebar_menu, hide_default_nav
-from respiratory_page_components import render_copd_tab, render_asthma_tab, render_pneumonia_tab
+from respiratory_page_components import render_copd_tab, render_asthma_tab, render_pneumonia_tab, render_chronic_cough_tab
 
 st.set_page_config(page_title="Hô Hấp", page_icon="🫁", layout="wide")
 
@@ -28,7 +28,7 @@ st.markdown(get_custom_css(dark_mode=st.session_state.dark_mode), unsafe_allow_h
 st.title("🫁 Tư vấn Hô Hấp")
 
 # Tabs cho các bệnh hô hấp
-tab1, tab2, tab3 = st.tabs(["🫁 COPD (Phổi Tắc Nghẽn)", "🌬️ Hen Suyễn", "🫁 Viêm phổi"])
+tab1, tab2, tab3, tab4 = st.tabs(["🫁 COPD (Phổi Tắc Nghẽn)", "🌬️ Hen Suyễn", "🫁 Viêm phổi", "🤧 Ho Mãn Tính"])
 
 with tab1:
     render_copd_tab()
@@ -38,6 +38,9 @@ with tab2:
 
 with tab3:
     render_pneumonia_tab()
+
+with tab4:
+    render_chronic_cough_tab()
 
 # Nút quay lại
 st.divider()
